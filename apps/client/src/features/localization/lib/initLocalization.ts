c7c35cf5eeda940b3543d2b51e46f67c65034d7b
Виталий <46228376+LayXL@users.noreply.github.com>
2024-09-03 19:51:55 +0500

    Code Styling

--- apps/client/src/features/localization/lib/initLocalization.ts
+++ apps/client/src/features/localization/lib/initLocalization.ts
@@ -5,27 +5,28 @@ import { initReactI18next } from "react-i18next"
 import { isTelegram } from "../../../shared/utils/platform-detect"
 
 export const activeLanguage = isTelegram
-    ? window.Telegram?.WebApp?.initDataUnsafe?.user?.language_code || "en"
-    : "ru"
+  ? window.Telegram?.WebApp?.initDataUnsafe?.user?.language_code || "en"
+  : "ru"
 
 export const activeLanguageId =
-    Object.entries({
-        en: 1,
-        ru: 2,
-    }).find(([x]) => x === activeLanguage)![1] ?? 1
+  Object.entries({
+    en: 1,
+    ru: 2,
+  }).find(([x]) => x === activeLanguage)![1] ?? 1
 
 export default function () {
-    i18n.use(initReactI18next)
-        .use(
-            resourcesToBackend(
-                (lang: string) => import(`../../../locales/${lang}.json`)
-            )
-        )
-        .init({
-            lng: activeLanguage,
-            fallbackLng: "en",
-            interpolation: { escapeValue: false },
-        })
+  i18n
+    .use(initReactI18next)
+    .use(
+      resourcesToBackend(
+        (lang: string) => import(`../../../locales/${lang}.json`)
+      )
+    )
+    .init({
+      lng: activeLanguage,
+      fallbackLng: "en",
+      interpolation: { escapeValue: false },
+    })
 
-    Settings.defaultLocale = activeLanguage
+  Settings.defaultLocale = activeLanguage
 }
