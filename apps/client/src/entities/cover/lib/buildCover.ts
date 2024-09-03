63a11163f3bb19ae2fa2059ce555b4255a88d3f2
Виталий <46228376+LayXL@users.noreply.github.com>
2024-09-04 02:11:13 +0500

    Refactoring

---
+++ apps/client/src/entities/cover/lib/buildCover.ts
@@ -0,0 +1,49 @@
+import type { Cover, DeepPartial } from "../../../shared/types"
+import { fillBackground } from "./fillBackground"
+
+export const buildCover = (
+  canvas: HTMLCanvasElement,
+  cover: DeepPartial<Cover>,
+  options?: { pixelRatio?: number; iconCanvas?: HTMLCanvasElement | null }
+) => {
+  const ctx = canvas.getContext("2d")
+
+  if (!ctx) return
+
+  const toRelativePx = (n: number) => n * (options?.pixelRatio ?? 1)
+
+  const iconSize = toRelativePx(cover.icon?.size ?? 32)
+  const fontSize = toRelativePx(cover.text?.fontSize ?? 16)
+
+  const iconCanvas = options?.iconCanvas
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  if (cover.bg) fillBackground(canvas, cover.bg)
+
+  if (cover.text?.value && cover.text.value.length > 0) {
+    ctx.fillStyle = cover.text.color ?? "#000"
+    ctx.font = `${fontSize}px sans-serif`
+    ctx.textAlign = "center"
+    ctx.textBaseline = "middle"
+
+    const offset = cover.icon ? iconSize / 2 + toRelativePx(4) : 0
+
+    const top = canvas.height / 2 + offset
+    const left = canvas.width / 2
+
+    ctx.fillText(cover.text.value, left, top)
+  }
+
+  if (cover.icon && iconCanvas) {
+    const offset =
+      cover.text?.value && cover.text.value.length > 0
+        ? fontSize / 2 + toRelativePx(4)
+        : 0
+
+    const top = canvas.height / 2 - iconSize / 2 - offset
+    const left = canvas.width / 2 - iconSize / 2
+
+    ctx.drawImage(iconCanvas, left, top, iconSize, iconSize)
+  }
+}
--- apps/client/src/entities/cover/ui/cover-renderer.tsx
+++ apps/client/src/entities/cover/ui/cover-renderer.tsx
@@ -1,79 +1,54 @@
-import { useCallback, useEffect, useRef } from "react"
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react"
 import type { Cover, DeepPartial } from "../../../shared/types"
-import { fillBackground } from "../lib/fillBackground"
+import { buildCover } from "../lib/buildCover"
 import { useIconCanvas } from "../lib/useIconCanvas"
 
 type CoverRendererProps = {
   pixelRatio?: number
 } & DeepPartial<Cover>
 
-export const CoverRenderer = (props: CoverRendererProps) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+export const CoverRenderer = forwardRef<HTMLCanvasElement, CoverRendererProps>(
+  (props, ref) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null)
 
-  const pixelRatio = props.pixelRatio ?? window.devicePixelRatio
+    const pixelRatio = props.pixelRatio ?? window.devicePixelRatio
 
-  const toRelativePx = useCallback((n: number) => n * pixelRatio, [pixelRatio])
+    const toRelativePx = useCallback(
+      (n: number) => n * pixelRatio,
+      [pixelRatio]
+    )
 
-  const iconSize = toRelativePx(props.icon?.size ?? 32)
-  const fontSize = toRelativePx(props.text?.fontSize ?? 16)
+    const iconCanvas = useIconCanvas(
+      props.icon?.name,
+      toRelativePx(props.icon?.size ?? 32),
+      props.icon?.color
+    )
 
-  const iconCanvas = useIconCanvas(
-    props.icon?.name,
-    iconSize,
-    props.icon?.color
-  )
+    useEffect(() => {
+      if (!canvasRef.current) return
 
-  useEffect(() => {
-    const canvas = canvasRef.current
-    const ctx = canvas?.getContext("2d")
+      buildCover(canvasRef.current, props, {
+        pixelRatio,
+        iconCanvas,
+      })
+    }, [props, iconCanvas, pixelRatio])
 
-    if (!canvas || !ctx) return
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    useImperativeHandle(ref, () => canvasRef.current!)
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    if (props.bg) fillBackground(canvas, props.bg)
-
-    if (props.text?.value && props.text.value.length > 0) {
-      ctx.fillStyle = props.text.color ?? "#000"
-      ctx.font = `${fontSize}px sans-serif`
-      ctx.textAlign = "center"
-      ctx.textBaseline = "middle"
-
-      const offset = props.icon ? iconSize / 2 + toRelativePx(4) : 0
-
-      const top = canvas.height / 2 + offset
-      const left = canvas.width / 2
-
-      ctx.fillText(props.text.value, left, top)
-    }
-
-    if (props.icon && iconCanvas) {
-      const offset =
-        props.text?.value && props.text.value.length > 0
-          ? fontSize / 2 + toRelativePx(4)
-          : 0
-
-      const top = canvas.height / 2 - iconSize / 2 - offset
-      const left = canvas.width / 2 - iconSize / 2
-
-      ctx.drawImage(iconCanvas, left, top, iconSize, iconSize)
-    }
-  }, [
-    props.bg,
-    props.text,
-    props.icon,
-    iconCanvas,
-    fontSize,
-    iconSize,
-    toRelativePx,
-  ])
-
-  return (
-    <canvas
-      width={376 * pixelRatio}
-      height={256 * pixelRatio}
-      ref={canvasRef}
-      className="size-full object-cover"
-    />
-  )
-}
+    return (
+      <canvas
+        width={376 * pixelRatio}
+        height={256 * pixelRatio}
+        ref={canvasRef}
+        className="size-full object-cover"
+      />
+    )
+  }
+)
