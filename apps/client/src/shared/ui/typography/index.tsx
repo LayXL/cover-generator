0dfee7e2395d5a31308c87c5556d028777dc641c
Виталий <46228376+LayXL@users.noreply.github.com>
2024-09-08 18:49:44 +0500

    fixes

--- apps/client/src/shared/ui/typography/index.tsx
+++ apps/client/src/shared/ui/typography/index.tsx
@@ -1,26 +1,33 @@
 import { cn } from "@/shared/utils/cn"
-import type { ReactNode } from "react"
+import { type ReactNode, memo } from "react"
 import { useTranslation } from "react-i18next"
 
 export type TypographyProps = {
   className?: string
 } & (
   | {
-      i18nKey: string
-      i18nOptions: Parameters<ReturnType<typeof useTranslation>["t"]>[2]
+      i18nKey?: string
+      i18nOptions?: Parameters<ReturnType<typeof useTranslation>["t"]>[2]
     }
   | {
-      children: ReactNode
+      children?: ReactNode
     }
 )
-export const Typography = (props: TypographyProps) => {
+export const Typography = memo((props: TypographyProps) => {
   const { t } = useTranslation()
 
   const children =
-    "children" in props ? props.children : t(props.i18nKey, props.i18nOptions)
+    "children" in props
+      ? props.children
+      : "i18nKey" in props &&
+          props.i18nKey &&
+          "i18nOptions" in props &&
+          props.i18nKey
+        ? t(props.i18nKey, props.i18nOptions)
+        : undefined
 
   return <p className={cn(props.className)}>{children}</p>
-}
+})
 
 export { Caption } from "./caption"
 export { Headline } from "./headline"
